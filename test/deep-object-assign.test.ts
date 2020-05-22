ddb6bfa634c58e17a455d97566d7efb0ce352edc
Tomáš Vyčítal <tom.vycital@gmail.com>
2020-05-22 23:35:50 +0200

    feat: add deep object assing with delete functionality
    It uses exported DELETE symbol so that we don't sacrifice any value.
    This should generally be a drop in replacement for the old merge logic
    however there may be some edge cases where it misbehaves.

---
+++ test/deep-object-assign.test.ts
@@ -0,0 +1,248 @@
+import { given, test } from "sazerac";
+
+import { deepObjectAssign, DELETE } from "../src";
+
+const SYMBOL_KEY = Symbol("key");
+const SYMBOL_ORIGINAL = Symbol("original");
+const SYMBOL_NEW = Symbol("new");
+
+test(deepObjectAssign, (): void => {
+  given({}).expect({});
+  given({}, {}).expect({});
+  given({}, {}, {}).expect({});
+
+  given({ zero: 0, two: 0 }, { one: 1, two: 2 }).expect({
+    zero: 0,
+    one: 1,
+    two: 2,
+  });
+
+  given({ zero: [0], two: [0, 0] }, { one: [1], two: [2] }).expect({
+    zero: [0],
+    one: [1],
+    two: [2],
+  });
+
+  given(
+    { zero: [0], two: [[0], 0] },
+    { one: [[1], { ONE: 1 }], two: [{ TWO: [2] }] }
+  ).expect({
+    zero: [0],
+    one: [[1], { ONE: 1 }],
+    two: [{ TWO: [2] }],
+  });
+
+  given(
+    { zero: 0, two: 0 },
+    { one: 1, two: 1 },
+    { two: 2 },
+    { nested: { threeThenOne: 1 } }
+  ).expect({
+    zero: 0,
+    one: 1,
+    two: 2,
+    nested: { threeThenOne: 1 },
+  });
+
+  given(
+    { zero: 0, two: 0 },
+    { one: 1, two: 1 },
+    { two: 2 },
+    { nested: { threeThenOne: 1 } }
+  ).expect({
+    zero: 0,
+    one: 1,
+    two: 2,
+    nested: { threeThenOne: 1 },
+  });
+
+  given(
+    {
+      bolean: true,
+      id: 0.25,
+      number: 37,
+      string: "oops",
+      symbol: SYMBOL_ORIGINAL,
+    },
+    {
+      bolean: true,
+      number: 42,
+      string: "yay",
+      symbol: SYMBOL_NEW,
+    }
+  ).expect({
+    bolean: true,
+    id: 0.25,
+    number: 42,
+    string: "yay",
+    symbol: SYMBOL_NEW,
+  });
+
+  given(
+    {
+      bolean: false,
+      id: 0.25,
+      number: Number.NaN,
+      string: "",
+      symbol: SYMBOL_ORIGINAL,
+    },
+    {
+      bolean: true,
+      number: 42,
+      string: "yay",
+      symbol: SYMBOL_NEW,
+    }
+  ).expect({
+    bolean: true,
+    id: 0.25,
+    number: 42,
+    string: "yay",
+    symbol: SYMBOL_NEW,
+  });
+
+  given(
+    {
+      bolean: true,
+      id: 0.25,
+      number: 4,
+      string: "oops",
+      symbol: SYMBOL_ORIGINAL,
+    },
+    {
+      bolean: false,
+      number: Number.NaN,
+      string: "",
+      symbol: SYMBOL_NEW,
+    }
+  ).expect({
+    bolean: false,
+    id: 0.25,
+    number: Number.NaN,
+    string: "",
+    symbol: SYMBOL_NEW,
+  });
+
+  given(
+    {
+      bolean: true,
+      id: 0.25,
+      number: 4,
+      string: "oops",
+      symbol: SYMBOL_ORIGINAL,
+    },
+    {
+      bolean: DELETE,
+      number: DELETE,
+      string: DELETE,
+      symbol: DELETE,
+    }
+  ).expect({
+    id: 0.25,
+  });
+
+  given(
+    {
+      id: 0.25,
+      one: {
+        two: {
+          three: {
+            four: "oops",
+          },
+        },
+      },
+    },
+    {
+      one: DELETE,
+    }
+  ).expect({
+    id: 0.25,
+  });
+
+  given(
+    {
+      id: 0.25,
+      one: {
+        two: {
+          three: {
+            four: "oops",
+          },
+        },
+      },
+    },
+    {
+      one: { miss: DELETE },
+    }
+  ).expect({
+    id: 0.25,
+    one: {
+      two: {
+        three: {
+          four: "oops",
+        },
+      },
+    },
+  });
+
+  given(
+    {
+      id: 0.25,
+      one: {
+        two: {
+          three: {
+            four: "oops",
+          },
+        },
+      },
+    },
+    {
+      double: { miss: DELETE },
+    }
+  ).expect({
+    id: 0.25,
+    one: {
+      two: {
+        three: {
+          four: "oops",
+        },
+      },
+    },
+    double: {},
+  });
+
+  given(
+    {
+      id: 0.25,
+      one: {
+        two: {
+          survivor: "yay",
+          three: {
+            four: "oops",
+          },
+        },
+      },
+    },
+    {
+      one: { two: { three: DELETE } },
+    }
+  ).expect({
+    id: 0.25,
+    one: {
+      two: {
+        survivor: "yay",
+      },
+    },
+  });
+
+  given(
+    {
+      id: 0.25,
+      [SYMBOL_KEY]: 1,
+    },
+    {
+      [SYMBOL_KEY]: 2,
+    }
+  ).expect({
+    id: 0.25,
+    [SYMBOL_KEY]: 2,
+  });
+});
