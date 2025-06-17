d72ca8633d3d061984978e3a48e7712490288ac1
Tomina <tomina@vycital.eu>
2025-06-17 18:54:02 +0200

    test(package): update snaphot to the new fully JSON format

--- __snapshots__/test/package.test.ts.js
+++ __snapshots__/test/package.test.ts.js
@@ -1,98 +1,284 @@
 exports['Package Exported files 1'] = {
-  "name": "vis-util                                ",
-  "files": [
-    "        LICENSE-APACHE-2.0",
-    "        LICENSE-MIT",
-    "        README.md",
-    "        declarations/deep-object-assign.d.ts",
-    "        declarations/deep-object-assign.d.ts.map",
-    "        declarations/entry-esnext.d.ts",
-    "        declarations/entry-esnext.d.ts.map",
-    "        declarations/entry-peer.d.ts",
-    "        declarations/entry-peer.d.ts.map",
-    "        declarations/entry-standalone.d.ts",
-    "        declarations/entry-standalone.d.ts.map",
-    "        declarations/index.d.ts",
-    "        declarations/index.d.ts.map",
-    "        declarations/random/alea.d.ts",
-    "        declarations/random/alea.d.ts.map",
-    "        declarations/random/index.d.ts",
-    "        declarations/random/index.d.ts.map",
-    "        declarations/shared/configurator-types.d.ts",
-    "        declarations/shared/configurator-types.d.ts.map",
-    "        declarations/shared/index.d.ts",
-    "        declarations/shared/index.d.ts.map",
-    "        declarations/util.d.ts",
-    "        declarations/util.d.ts.map",
-    "        esnext/esm/index.d.ts",
-    "        esnext/esm/index.js",
-    "        esnext/esm/vis-util.d.ts",
-    "        esnext/esm/vis-util.js",
-    "        esnext/esm/vis-util.js.map",
-    "        esnext/esm/vis-util.min.d.ts",
-    "        esnext/esm/vis-util.min.js",
-    "        esnext/esm/vis-util.min.js.map",
-    "        esnext/index.d.ts",
-    "        esnext/index.js",
-    "        esnext/styles/activator.css",
-    "        esnext/styles/bootstrap.css",
-    "        esnext/styles/color-picker.css",
-    "        esnext/styles/configurator.css",
-    "        esnext/styles/popup.css",
-    "        esnext/umd/index.d.ts",
-    "        esnext/umd/index.js",
-    "        esnext/umd/vis-util.d.ts",
-    "        esnext/umd/vis-util.js",
-    "        esnext/umd/vis-util.js.map",
-    "        esnext/umd/vis-util.min.d.ts",
-    "        esnext/umd/vis-util.min.js",
-    "        esnext/umd/vis-util.min.js.map",
-    "        package.json",
-    "        peer/esm/index.d.ts",
-    "        peer/esm/index.js",
-    "        peer/esm/vis-util.d.ts",
-    "        peer/esm/vis-util.js",
-    "        peer/esm/vis-util.js.map",
-    "        peer/esm/vis-util.min.d.ts",
-    "        peer/esm/vis-util.min.js",
-    "        peer/esm/vis-util.min.js.map",
-    "        peer/index.d.ts",
-    "        peer/index.js",
-    "        peer/styles/activator.css",
-    "        peer/styles/bootstrap.css",
-    "        peer/styles/color-picker.css",
-    "        peer/styles/configurator.css",
-    "        peer/styles/popup.css",
-    "        peer/umd/index.d.ts",
-    "        peer/umd/index.js",
-    "        peer/umd/vis-util.d.ts",
-    "        peer/umd/vis-util.js",
-    "        peer/umd/vis-util.js.map",
-    "        peer/umd/vis-util.min.d.ts",
-    "        peer/umd/vis-util.min.js",
-    "        peer/umd/vis-util.min.js.map",
-    "        standalone/esm/index.d.ts",
-    "        standalone/esm/index.js",
-    "        standalone/esm/vis-util.d.ts",
-    "        standalone/esm/vis-util.js",
-    "        standalone/esm/vis-util.js.map",
-    "        standalone/esm/vis-util.min.d.ts",
-    "        standalone/esm/vis-util.min.js",
-    "        standalone/esm/vis-util.min.js.map",
-    "        standalone/index.d.ts",
-    "        standalone/index.js",
-    "        standalone/styles/activator.css",
-    "        standalone/styles/bootstrap.css",
-    "        standalone/styles/color-picker.css",
-    "        standalone/styles/configurator.css",
-    "        standalone/styles/popup.css",
-    "        standalone/umd/index.d.ts",
-    "        standalone/umd/index.js",
-    "        standalone/umd/vis-util.d.ts",
-    "        standalone/umd/vis-util.js",
-    "        standalone/umd/vis-util.js.map",
-    "        standalone/umd/vis-util.min.d.ts",
-    "        standalone/umd/vis-util.min.js",
-    "        standalone/umd/vis-util.min.js.map"
-  ]
+  "name": "vis-util",
+  "files": {
+    "LICENSE-APACHE-2.0": {
+      "empty": false
+    },
+    "LICENSE-MIT": {
+      "empty": false
+    },
+    "README.md": {
+      "empty": false
+    },
+    "declarations/deep-object-assign.d.ts": {
+      "empty": false
+    },
+    "declarations/deep-object-assign.d.ts.map": {
+      "empty": false
+    },
+    "declarations/entry-esnext.d.ts": {
+      "empty": false
+    },
+    "declarations/entry-esnext.d.ts.map": {
+      "empty": false
+    },
+    "declarations/entry-peer.d.ts": {
+      "empty": false
+    },
+    "declarations/entry-peer.d.ts.map": {
+      "empty": false
+    },
+    "declarations/entry-standalone.d.ts": {
+      "empty": false
+    },
+    "declarations/entry-standalone.d.ts.map": {
+      "empty": false
+    },
+    "declarations/index.d.ts": {
+      "empty": false
+    },
+    "declarations/index.d.ts.map": {
+      "empty": false
+    },
+    "declarations/random/alea.d.ts": {
+      "empty": false
+    },
+    "declarations/random/alea.d.ts.map": {
+      "empty": false
+    },
+    "declarations/random/index.d.ts": {
+      "empty": false
+    },
+    "declarations/random/index.d.ts.map": {
+      "empty": false
+    },
+    "declarations/shared/configurator-types.d.ts": {
+      "empty": false
+    },
+    "declarations/shared/configurator-types.d.ts.map": {
+      "empty": false
+    },
+    "declarations/shared/index.d.ts": {
+      "empty": false
+    },
+    "declarations/shared/index.d.ts.map": {
+      "empty": false
+    },
+    "declarations/util.d.ts": {
+      "empty": false
+    },
+    "declarations/util.d.ts.map": {
+      "empty": false
+    },
+    "esnext/esm/index.d.ts": {
+      "empty": false
+    },
+    "esnext/esm/index.js": {
+      "empty": false
+    },
+    "esnext/esm/vis-util.d.ts": {
+      "empty": false
+    },
+    "esnext/esm/vis-util.js": {
+      "empty": false
+    },
+    "esnext/esm/vis-util.js.map": {
+      "empty": false
+    },
+    "esnext/esm/vis-util.min.d.ts": {
+      "empty": false
+    },
+    "esnext/esm/vis-util.min.js": {
+      "empty": false
+    },
+    "esnext/esm/vis-util.min.js.map": {
+      "empty": false
+    },
+    "esnext/index.d.ts": {
+      "empty": false
+    },
+    "esnext/index.js": {
+      "empty": false
+    },
+    "esnext/styles/activator.css": {
+      "empty": false
+    },
+    "esnext/styles/bootstrap.css": {
+      "empty": false
+    },
+    "esnext/styles/color-picker.css": {
+      "empty": false
+    },
+    "esnext/styles/configurator.css": {
+      "empty": false
+    },
+    "esnext/styles/popup.css": {
+      "empty": false
+    },
+    "esnext/umd/index.d.ts": {
+      "empty": false
+    },
+    "esnext/umd/index.js": {
+      "empty": false
+    },
+    "esnext/umd/vis-util.d.ts": {
+      "empty": false
+    },
+    "esnext/umd/vis-util.js": {
+      "empty": false
+    },
+    "esnext/umd/vis-util.js.map": {
+      "empty": false
+    },
+    "esnext/umd/vis-util.min.d.ts": {
+      "empty": false
+    },
+    "esnext/umd/vis-util.min.js": {
+      "empty": false
+    },
+    "esnext/umd/vis-util.min.js.map": {
+      "empty": false
+    },
+    "package.json": {
+      "empty": false
+    },
+    "peer/esm/index.d.ts": {
+      "empty": false
+    },
+    "peer/esm/index.js": {
+      "empty": false
+    },
+    "peer/esm/vis-util.d.ts": {
+      "empty": false
+    },
+    "peer/esm/vis-util.js": {
+      "empty": false
+    },
+    "peer/esm/vis-util.js.map": {
+      "empty": false
+    },
+    "peer/esm/vis-util.min.d.ts": {
+      "empty": false
+    },
+    "peer/esm/vis-util.min.js": {
+      "empty": false
+    },
+    "peer/esm/vis-util.min.js.map": {
+      "empty": false
+    },
+    "peer/index.d.ts": {
+      "empty": false
+    },
+    "peer/index.js": {
+      "empty": false
+    },
+    "peer/styles/activator.css": {
+      "empty": false
+    },
+    "peer/styles/bootstrap.css": {
+      "empty": false
+    },
+    "peer/styles/color-picker.css": {
+      "empty": false
+    },
+    "peer/styles/configurator.css": {
+      "empty": false
+    },
+    "peer/styles/popup.css": {
+      "empty": false
+    },
+    "peer/umd/index.d.ts": {
+      "empty": false
+    },
+    "peer/umd/index.js": {
+      "empty": false
+    },
+    "peer/umd/vis-util.d.ts": {
+      "empty": false
+    },
+    "peer/umd/vis-util.js": {
+      "empty": false
+    },
+    "peer/umd/vis-util.js.map": {
+      "empty": false
+    },
+    "peer/umd/vis-util.min.d.ts": {
+      "empty": false
+    },
+    "peer/umd/vis-util.min.js": {
+      "empty": false
+    },
+    "peer/umd/vis-util.min.js.map": {
+      "empty": false
+    },
+    "standalone/esm/index.d.ts": {
+      "empty": false
+    },
+    "standalone/esm/index.js": {
+      "empty": false
+    },
+    "standalone/esm/vis-util.d.ts": {
+      "empty": false
+    },
+    "standalone/esm/vis-util.js": {
+      "empty": false
+    },
+    "standalone/esm/vis-util.js.map": {
+      "empty": false
+    },
+    "standalone/esm/vis-util.min.d.ts": {
+      "empty": false
+    },
+    "standalone/esm/vis-util.min.js": {
+      "empty": false
+    },
+    "standalone/esm/vis-util.min.js.map": {
+      "empty": false
+    },
+    "standalone/index.d.ts": {
+      "empty": false
+    },
+    "standalone/index.js": {
+      "empty": false
+    },
+    "standalone/styles/activator.css": {
+      "empty": false
+    },
+    "standalone/styles/bootstrap.css": {
+      "empty": false
+    },
+    "standalone/styles/color-picker.css": {
+      "empty": false
+    },
+    "standalone/styles/configurator.css": {
+      "empty": false
+    },
+    "standalone/styles/popup.css": {
+      "empty": false
+    },
+    "standalone/umd/index.d.ts": {
+      "empty": false
+    },
+    "standalone/umd/index.js": {
+      "empty": false
+    },
+    "standalone/umd/vis-util.d.ts": {
+      "empty": false
+    },
+    "standalone/umd/vis-util.js": {
+      "empty": false
+    },
+    "standalone/umd/vis-util.js.map": {
+      "empty": false
+    },
+    "standalone/umd/vis-util.min.d.ts": {
+      "empty": false
+    },
+    "standalone/umd/vis-util.min.js": {
+      "empty": false
+    },
+    "standalone/umd/vis-util.min.js.map": {
+      "empty": false
+    },
+  }
 }
